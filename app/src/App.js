66e067a54fdb9f40f347be23a55747a6be9c1b1f
thoma <thomashsu0512@gmail.com>
2025-01-07 10:54:17 -0500

    Added Tailwind

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-
 import React from 'react'
 
 export default function App() {
+    const apiKey = process.env.REACT_APP_API_KEY;
 
-  return (
-      <div>
-        hi
-      </div>
-  );
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
